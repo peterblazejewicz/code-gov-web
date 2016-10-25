983280b659c2469ecf6c547aa470eb51356384ad
Olivier Kamanda <olivier.kamanda@pif.gov>
2016-10-25 11:44:02 -0400

    Draft of events feed
    let me know if this is what you had in mind.

---
+++ src/app/services/events.data.ts
@@ -0,0 +1,138 @@
+export const EVENTS = [
+
+{
+     id: '33202667',
+     name: 'department-of-veterans-affairs/vets-website',
+     type:'IssueComment',
+     user:'webinista',
+     time:'2016-10-25T15:29:36Z',
+     message: 'Updated URL routes in Rx and messaging.',
+      url:'https://api.github.com/repos/department-of-veterans-affairs/vets-website/issues/3512'
+},{
+     id: '33202667',
+     name: 'department-of-veterans-affairs/vets-website',
+     type:'PullRequest',
+     user:'crwallace',
+     time:'2016-10-25T15:18:43Z',
+     message: 'removes 1010AD',
+      url:'https://api.github.com/repos/department-of-veterans-affairs/vets-website/pulls/3526'
+},{
+     id: '33202667',
+     name: 'department-of-veterans-affairs/vets-website',
+     type:'Create',
+     user:'crwallace',
+     time:'2016-10-25T15:17:36Z',
+
+},{
+     id: '33202667',
+     name: 'department-of-veterans-affairs/vets-website',
+     type:'Push',
+     user:'U-DON',
+     time:'2016-10-25T15:13:58Z',
+     message: 'adds a link for loa3 to global sign in for development (#3523) * adds a link for loa3 to global sign in for development * chnages json structure to new schema * fixes tests',
+      url:'https://api.github.com/repos/department-of-veterans-affairs/vets-website/commits/53ffc860595ee42bd1d002b6b320655393fdf10a'
+},{
+     id: '33202667',
+     name: 'department-of-veterans-affairs/vets-website',
+     type:'Push',
+     user:'U-DON',
+     time:'2016-10-25T15:00:37Z',
+     message: 'adds a link for loa3 to global sign in for development (#3523) * adds a link for loa3 to global sign in for development * chnages json structure to new schema * fixes tests',
+      url:'https://api.github.com/repos/department-of-veterans-affairs/vets-website/commits/53ffc860595ee42bd1d002b6b320655393fdf10a'
+},{
+     id: '33202667',
+     name: 'department-of-veterans-affairs/vets-website',
+     type:'Push',
+     user:'U-DON',
+     time:'2016-10-25T14:59:42Z',
+     message: 'Fix scroll handling on claims status (#3509)',
+      url:'https://api.github.com/repos/department-of-veterans-affairs/vets-website/commits/bcf7abb63e5af5e084c41951769c3da808437919'
+},{
+     id: '57156495',
+     name: '18F/vote-gov',
+     type:'Issues',
+     user:'jamesharnett',
+     time:'2016-10-19T18:00:42Z',
+
+},{
+     id: '57156495',
+     name: '18F/vote-gov',
+     type:'IssueComment',
+     user:'arantxarecio',
+     time:'2016-10-17T21:42:29Z',
+     message: 'Add statement about voter reg deadlines to state pages',
+      url:'https://api.github.com/repos/18F/vote-gov/issues/151'
+},{
+     id: '57156495',
+     name: '18F/vote-gov',
+     type:'Issues',
+     user:'megdaly',
+     time:'2016-10-17T18:15:22Z',
+
+},{
+     id: '57156495',
+     name: '18F/vote-gov',
+     type:'Issues',
+     user:'yozlet',
+     time:'2016-10-11T20:28:57Z',
+
+},{
+     id: '57156495',
+     name: '18F/vote-gov',
+     type:'IssueComment',
+     user:'yozlet',
+     time:'2016-10-11T20:28:57Z',
+     message: 'Launch checklist',
+      url:'https://api.github.com/repos/18F/vote-gov/issues/121'
+},{
+     id: '57156495',
+     name: '18F/vote-gov',
+     type:'Push',
+     user:'yozlet',
+     time:'2016-10-11T18:16:57Z',
+     message: 'Add redirector nginx conf',
+      url:'https://api.github.com/repos/18F/vote-gov/commits/91b79a23d061b5117446c60a6b48f1d4a4ac1254'
+},
+{
+     id: '70825196',
+     name: 'WhiteHouse/fb_messenger_bot',
+     type:'Fork',
+     user:'xsongx',
+     time:'2016-10-25T15:26:48Z',
+
+},{
+     id: '70825196',
+     name: 'WhiteHouse/fb_messenger_bot',
+     type:'Watch',
+     user:'levelos',
+     time:'2016-10-25T15:09:48Z',
+
+},{
+     id: '70825196',
+     name: 'WhiteHouse/fb_messenger_bot',
+     type:'Fork',
+     user:'youngshine',
+     time:'2016-10-25T14:26:30Z',
+
+},{
+     id: '70825196',
+     name: 'WhiteHouse/fb_messenger_bot',
+     type:'Fork',
+     user:'XiliangSong',
+     time:'2016-10-25T11:01:10Z',
+
+},{
+     id: '70825196',
+     name: 'WhiteHouse/fb_messenger_bot',
+     type:'Watch',
+     user:'XiliangSong',
+     time:'2016-10-25T11:01:07Z',
+
+},{
+     id: '70825196',
+     name: 'WhiteHouse/fb_messenger_bot',
+     type:'Fork',
+     user:'happyqq',
+     time:'2016-10-25T08:08:06Z',
+
+}];
